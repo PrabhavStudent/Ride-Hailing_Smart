8c57885c92f7d43a054bbe357378f4975a807e8a
Prabhav Singh <prabhavnirwal01@gmail.com>
2025-05-12 23:36:30 +0000

    error fixed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const request = require('request'); // Still using this old friend; yeah I know it's deprecated
+const request = require('request'); 
 
 // Custom modules
 const { loadData } = require('./data');
